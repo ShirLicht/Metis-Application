61260fd261a6700ce43596c9c2d726c88e56734e
Maor Shapira <maor.shapira@gmail.com>
2018-08-02 17:39:41 +0300

    Firebase + Database

--- app/src/main/java/com/example/admin/metis/FoodMenuFragment.java
+++ app/src/main/java/com/example/admin/metis/FoodMenuFragment.java
@@ -10,11 +10,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class FoodMenuFragment extends Fragment {
+
+   private  final String DB_Url = "https://metis-application.firebaseio.com/";
+   private DatabaseReference mRef;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        mRef  = FirebaseDatabase.getInstance().getReferenceFromUrl(DB_Url);
+        DatabaseReference mRefChild = mRef.child("maor");
+        mRefChild.setValue("pitta");
+
         return inflater.inflate(R.layout.fragment_food_menu, container, false);
     }
 }
--- app/src/main/java/com/example/admin/metis/MainActivity.java
+++ app/src/main/java/com/example/admin/metis/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.admin.metis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.idButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, BarMActivity.class );
+                startActivity(intent);
+            }
+        });
     }
 }
