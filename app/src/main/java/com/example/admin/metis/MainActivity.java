09e0d783d6b35c64387f1ccdcd404adc2c6b6f3a
Maor Shapira <maor.shapira@gmail.com>
2018-08-06 21:22:32 +0300

    after Facebook integration

--- app/src/main/java/com/example/admin/metis/MainActivity.java
+++ app/src/main/java/com/example/admin/metis/MainActivity.java
@@ -163,8 +163,6 @@ public class MainActivity extends AppCompatActivity {
                                     Toast.LENGTH_SHORT).show();
                             updateUI();
                         }
-
-                        // ...
                     }
                 });
     }
