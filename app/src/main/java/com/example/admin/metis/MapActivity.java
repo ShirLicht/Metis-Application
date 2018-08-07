cb2cca22999e046045abc1282f3a1d607f0d78cf
Maor Shapira <maor.shapira@gmail.com>
2018-08-07 14:25:55 +0300

    created MapActivity - version 1 fixed error in xml file

--- app/src/main/java/com/example/admin/metis/MapActivity.java
+++ app/src/main/java/com/example/admin/metis/MapActivity.java
@@ -1,42 +1,66 @@
 package com.example.admin.metis;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.facebook.login.LoginManager;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private FirebaseAuth mAuth;
+    private FirebaseAuth firebaseAuth;
     private Button logoutBtn;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        mAuth = FirebaseAuth.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+
         logoutBtn = (Button)findViewById(R.id.SignOutBtn);
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view){
-                mAuth.signOut();//log out from firebase
+               firebaseAuth.signOut();//log out from firebase
                 LoginManager.getInstance().logOut();//log out from facebook
                 Intent intent = new Intent(MapActivity.this,MainActivity.class );
                 startActivity(intent);
             }
         });
 
-
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
     }
 
+
     public void onStart(){
         super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
