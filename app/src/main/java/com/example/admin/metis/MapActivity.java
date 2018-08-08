c8a739616cb0871090f83bde35ef3ddd2c755b2f
ShirLicht <licht.shir@gmail.com>
2018-08-08 15:35:08 +0300

    Facebook - get user name and profile picture

--- app/src/main/java/com/example/admin/metis/MapActivity.java
+++ app/src/main/java/com/example/admin/metis/MapActivity.java
@@ -1,27 +1,41 @@
 package com.example.admin.metis;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 
 public class MapActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private Button logoutBtn;
+    private String userName, providerId;
+    private Uri userPhotoUrl;
+    private TextView userNameTxt;
+    private de.hdodenhof.circleimageview.CircleImageView userProfilePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        bindUI();
+        getUserInfo();
+
+        //user name & profile image from facebook account
+        userNameTxt.setText(userName);
+        userProfilePic.setImageURI(userPhotoUrl);
+
         mAuth = FirebaseAuth.getInstance();
-        logoutBtn = (Button)findViewById(R.id.SignOutBtn);
+
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view){
                 mAuth.signOut();//log out from firebase
@@ -39,4 +53,30 @@ public class MapActivity extends AppCompatActivity {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
     }
+
+    public void getUserInfo()
+    {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            for (UserInfo profile : user.getProviderData()) {
+                // Id of the provider (ex: google.com)
+                providerId = profile.getProviderId();
+
+                // UID specific to the provider
+                //String uid = profile.getUid();
+
+                // Name and profile photo Url
+                userName = profile.getDisplayName();
+                userPhotoUrl = profile.getPhotoUrl();
+            }
+            ;
+        }
+    }
+
+    public void bindUI(){
+        userNameTxt = (TextView)findViewById(R.id.UserNameTxtView);
+        userProfilePic = (de.hdodenhof.circleimageview.CircleImageView) findViewById(R.id.profile_image);
+        logoutBtn = (Button)findViewById(R.id.SignOutBtn);
+    }
+
 }
