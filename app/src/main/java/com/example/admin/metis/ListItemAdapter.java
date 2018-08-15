87c261b31d157f4dee403daf12c5670b53165582
ShirLicht <licht.shir@gmail.com>
2018-08-15 22:07:34 +0300

    Fixed General Design + Fixed ListItemAdapter (switch for layouts + fonts for the layouts items)

--- app/src/main/java/com/example/admin/metis/ListItemAdapter.java
+++ app/src/main/java/com/example/admin/metis/ListItemAdapter.java
@@ -2,6 +2,7 @@ package com.example.admin.metis;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -13,12 +14,15 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.facebook.FacebookSdk.getApplicationContext;
+
 public class ListItemAdapter extends ArrayAdapter<Product>{
 
     enum VIEW_SOURCE {TABLE_SOURCE, MENU_SOURCE}
 
     private final static String TAG = "Metis-Application: ";
     private VIEW_SOURCE view_source;
+    private Typeface topicFont, headerFont, itemFont;
 
     private Context context;
     private List<Product> itemsList;
@@ -35,10 +39,18 @@ public class ListItemAdapter extends ArrayAdapter<Product>{
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View listItem = convertView;
         TextView priceTextView;
+        setFont();
 
-        if(listItem == null)
-            listItem = LayoutInflater.from(context).inflate(R.layout.list_bar_item,parent,false);
-
+       // if(listItem == null)
+            //listItem = LayoutInflater.from(context).inflate(R.layout.menu_list_bar_item,parent,false);
+        switch(this.view_source){
+            case MENU_SOURCE:
+                listItem = LayoutInflater.from(context).inflate(R.layout.menu_list_bar_item,parent,false);
+                break;
+            case TABLE_SOURCE:
+                listItem = LayoutInflater.from(context).inflate(R.layout.table_list_bar_item,parent,false);
+                break;
+        }
         Product currentItem = itemsList.get(position);
         TextView nameTextView = listItem.findViewById(R.id.list_item_name);
         nameTextView.setText(currentItem.getName());
@@ -50,9 +62,11 @@ public class ListItemAdapter extends ArrayAdapter<Product>{
                 listItem.setPadding(0,0,0,0);
                 nameTextView.setTextSize(20);
                 nameTextView.setTextColor(Color.BLACK);
+                nameTextView.setTypeface(itemFont);
                 priceTextView = listItem.findViewById(R.id.list_item_price);
                 priceTextView.setText(currentItem.getPrice());
                 priceTextView.setTextColor(Color.BLACK);
+                priceTextView.setTypeface(itemFont);
                 break;
 
             case HEADER:
@@ -60,6 +74,7 @@ public class ListItemAdapter extends ArrayAdapter<Product>{
                 listItem.setPadding(0,50,0,0);
                 nameTextView.setTextSize(40);
                 nameTextView.setTextColor(Color.RED);
+                nameTextView.setTypeface(headerFont);
                 priceTextView = listItem.findViewById(R.id.list_item_price);
                 priceTextView.setText(" ");
                 break;
@@ -68,6 +83,7 @@ public class ListItemAdapter extends ArrayAdapter<Product>{
                 listItem.setPadding(0,50,0,0);
                 nameTextView.setTextSize(40);
                 nameTextView.setTextColor(Color.BLUE);
+                nameTextView.setTypeface(topicFont);
                 priceTextView = listItem.findViewById(R.id.list_item_price);
                 priceTextView.setText(" ");
                 break;
@@ -75,4 +91,11 @@ public class ListItemAdapter extends ArrayAdapter<Product>{
 
         return listItem;
     }
+
+    private void setFont(){
+        topicFont = Typeface.createFromAsset(getApplicationContext().getAssets(), "font/BreeSerif-Regular.ttf");
+        headerFont = Typeface.createFromAsset(getApplicationContext().getAssets(), "font/Jua-Regular.ttf");
+        itemFont = Typeface.createFromAsset(getApplicationContext().getAssets(), "font/GloriaHallelujah.ttf");
+    }
+
 }
