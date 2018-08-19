2629f45bb873f40eaf82842248d4f5063d8b8b5d
Maor Shapira <maor.shapira@gmail.com>
2018-08-19 18:08:07 +0300

    Pushed fixed PersonalReviewFragment

--- app/src/main/java/com/example/admin/metis/PersonalReviewFragment.java
+++ app/src/main/java/com/example/admin/metis/PersonalReviewFragment.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,6 +23,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static com.example.admin.metis.MenuActivity.BAR_NAME;
 
@@ -29,12 +32,15 @@ public class PersonalReviewFragment extends Fragment {
 
     private static final String REVIEW_NODE = "Reviews";
     private static final String REVIEWS_COUNTER_NODE = "ReviewsCounter";
+    private final static String TAG = "Metis-Application: ";
 
     private String review, reviewTitle, userId, userName, providerId;
     private Uri userPhotoUrl;
     private EditText reviewEditText, rateEditText;
-    private int rate, currentReviewNum;
+    private int rate;
     private View view;
+    private Button submitBtn;
+    private static int currentReviewNum;
 
     //Firebase Variables
     private FirebaseAuth firebaseAuth;
@@ -55,35 +61,44 @@ public class PersonalReviewFragment extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_personal_review, container, false);
+        firebaseDatabase = FirebaseDatabase.getInstance();
         bindUI();
-        saveReviewToDB();
+        setButtonsActions();
+        getUserInfo();
         return view;
     }
 
     private void bindUI(){
         reviewEditText = view.findViewById(R.id.reviewEditText);
         rateEditText = view.findViewById(R.id.starsEditText);
-
-        review = reviewEditText.getText().toString();
-        rate = Integer.parseInt(rateEditText.getText().toString());
+        submitBtn = view.findViewById(R.id.submitBtn);
     }
 
-    private void saveReviewToDB(){
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference= firebaseDatabase.getReference().child(BAR_NAME).child(REVIEW_NODE).child(REVIEWS_COUNTER_NODE);
+    private void setButtonsActions(){
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    review = reviewEditText.getText().toString();
+                    if(review.equals(""))
+                        return;
+                    rate = Integer.parseInt(rateEditText.getText().toString());
+                    saveReviewToDB();
+                }
+                catch(Exception ex) {
 
-        getReviewCounterFromDB();
+                }
+            }
+        });
+    }
 
-        //Raise the review number by 1 and update the counter in the DB
-        currentReviewNum++;
-        databaseReference.setValue(currentReviewNum);
 
-        //Generate a new review title
-        reviewTitle = "Review " + currentReviewNum;
+
+
+    private void saveReview(String reviewTitle){
 
         //Create and save a new review in to the DB
-        databaseReference= firebaseDatabase.getReference().child(BAR_NAME).child(REVIEW_NODE).child(reviewTitle);
-
+        DatabaseReference ref = firebaseDatabase.getReference().child(BAR_NAME).child(REVIEW_NODE).child(reviewTitle);
 
         //Data to save to DB
         HashMap<String, String> userMapData = new HashMap<>();
@@ -92,18 +107,25 @@ public class PersonalReviewFragment extends Fragment {
         userMapData.put("review", review + "");
         userMapData.put("rate", rate + "");
 
-        databaseReference.setValue(userMapData);
+        ref.setValue(userMapData);
     }
 
-    private void getReviewCounterFromDB(){
+    private void saveReviewToDB(){
+        databaseReference = firebaseDatabase.getReference().child(BAR_NAME).child(REVIEW_NODE);
+
         databaseReference.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                String reviewsCounter = dataSnapshot.getValue().toString();
-
-                currentReviewNum = Integer.parseInt(reviewsCounter);
-
-
+               if(dataSnapshot.getKey().equals(REVIEWS_COUNTER_NODE))
+               {
+                   String reviewsCounter = dataSnapshot.getValue().toString();
+                   currentReviewNum = Integer.parseInt(reviewsCounter);
+                   currentReviewNum++;
+                   databaseReference.child(REVIEWS_COUNTER_NODE).setValue(currentReviewNum+ "");
+                   saveReview("Review " + currentReviewNum);
+                   reviewEditText.setText("");
+                   rateEditText.setText("");
+               }
             }
 
             @Override
